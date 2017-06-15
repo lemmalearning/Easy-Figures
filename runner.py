9ab672a57d05a334938c620d431f3c92fddab1ab
Axel-Jose Persinger <me@ajpersinger.com>
2017-06-14 21:43:29 -0400

    Restructured shapes package, fixed font alignment in Circle

---
+++ runner.py
@@ -0,0 +1,57 @@
+import numpy as np
+from random import randint
+import matplotlib
+matplotlib.use('tkagg')
+import matplotlib.pyplot as plt
+from matplotlib import patches
+import figures
+
+def angle_builder(angle=(45*np.pi)/180, rotation=0, translation=(0,0), text=""): # Given a specific angle, generate a right triangle with one angle specified
+	fig, ax = plt.subplots()
+
+	# Define the angles and sides
+	alpha = angle
+	beta = np.pi/2
+	gamma = ((np.pi-beta-alpha))
+
+	A = 1
+	B = np.sin(beta)/np.sin(alpha)
+	C = np.sin(gamma)/np.sin(alpha)
+
+	# Define the vertices
+	vertex_A = [0, A]
+	vertex_B = [0, 0]
+	vertex_C = [C, 0]
+
+	polygon = plt.Polygon([vertex_A, vertex_B, vertex_C], fill=False, linewidth=3)
+
+	ax.text(1, 1, '$\\frac{1}{2}$', fontsize=15)
+
+	# Perform the rotation if at all
+	transformation = matplotlib.transforms.Affine2D().translate(translation[0], translation[1]).rotate(rotation) + ax.transData
+	polygon.set_transform(transformation)
+
+	# Create and add polygon
+	ax.add_patch(polygon)
+
+
+	# Modify the plot view to scale, remove axis, and center our shape
+	ax.autoscale_view()
+	plt.axis('off')
+	plt.axis('scaled')
+
+	print alpha, beta, gamma, A, B, C
+	# Display or Save to file
+	#plt.savefig('/Users/ajpersinger/test.svg')
+	plt.show()
+
+def main():
+	t = figures.Figures()
+	poly = t.addCircle(xy=(5,4),label="Q", diameter=1)
+
+	t.__writeFile__('/Users/ajpersinger/test.svg')
+
+	return None
+
+if __name__ == "__main__":
+	main()
---
+++ shapes/Polygon.py
@@ -0,0 +1,22 @@
+import matplotlib
+matplotlib.use('tkagg')
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+import numpy as np
+
+class Polygon:
+	def __init__(self, vertices, fig, ax):
+		self.vertices = vertices
+		self.fig = fig
+		self.ax = ax
+		# Define the polygon
+		polygon = plt.Polygon(vertices, fill=False, linewidth=3)
+		# Create and add polygon
+		ax.add_patch(polygon)
+
+	def labelVertices(self, label_list):
+		# Everything is the counter clockwise, and the first angle/vertex is the first lable, everything else is counter clockwise order
+		# The side that's mentioned first is horizontal
+		self.labels = label_list
+		for i, label in enumerate(self.labels):
+			self.ax.text(self.vertices[i, 0], self.vertices[i, 1], '$'+label+'$', fontsize=15)
