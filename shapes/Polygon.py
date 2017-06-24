1efe6f7b8e983025ed78b829014c87ac8e2d18ba
Dennis Shtatnov <densht@gmail.com>
2017-06-23 22:35:46 -0400

    Use angle bisector line for labeling

--- shapes/Polygon.py
+++ shapes/Polygon.py
@@ -40,35 +40,63 @@ class Polygon:
 			self.figure.addText((x,y), self.sideLabels[i], fontsize=.0625*self.pixelSize, latex=True,
 				halignment=hA,valignment=vA)
 
-	def labelVertices(self, labelList):
+
+	def bisector(self, i):
+		"""For a vertex, get the vector of the angle bisector (pointing inwards)"""
+		ab = self.vertices[i-1, :] - self.vertices[i, :]
+		ac = self.vertices[(i+1) % self.vertices.shape[0], :] - self.vertices[i, :]
+		bisec = (ab + ac) / 2.0 # This points inward
+		bisec = bisec / np.linalg.norm(bisec)
+		return bisec
+
+	def labelVertices(self, labelList, inner=False):
 		# Everything is the counter clockwise, and the first angle/vertex is the first lable, everything else is counter clockwise order
 		# The side that's mentioned first is horizontal
 		self.labels = labelList
 
-		centroid = np.mean(self.vertices, axis=0)
-
 		for i, label in enumerate(self.labels):
-			d = self.vertices[i, :] - centroid
-			v = self.vertices[i, :] + 0.001*np.linalg.norm(d)*d
 
-			# v[0, 0], v[0, 1]
+			d = self.bisector(i)
+			if not inner:
+				d = -d
 
-			txt = self.ax.text(v[0,0], v[0,1], '$'+label+'$', fontsize=20, \
-				#horizontalalignment=("right" if d[0,0] < 0 else "left"), \
-				#verticalalignment=("top" if d[0, 1] < 0 else "bottom") \
-			)
+			# TODO: We need to figure out a better amount of padding to use
+			p = 0.4 if inner else 0.1
+			v = self.vertices[i, :] + p*np.linalg.norm(d)*d
+
+			dx, dy = d[0, 0], d[0, 1]
+			vx, vy = v[0, 0], v[0, 1]
+
+			txt = self.ax.text(0, 0, '$'+label+'$', fontsize=10)
 
 			w, h = self.figure.measureText(txt, True)
 
-			if d[0, 0] < 0:
-				v[0, 0] -= w
-			if d[0, 1] < 0:
-				v[0, 1] -= h
+			# Next we essentially adjust the text to be anchored on a point anchored along the direction line that divides the rectangle into two equal length areas
+			# Center text on the direction line : this assumes that the text is usually anchored on the bottom left point
+			if abs(dy) > abs(dx): # run greater than rise : intersects top and bottom lines of rectangle
+				vx -= w / 2.0
+				delx = ((dx / dy) * h) / 2.0
+				if dy < 0:
+					vx -= delx
+					vy -= h
+				else:
+					vx += delx
+			else:
+				vy -= h / 2.0
+				dely = ((dy / dx) * w) / 2.0
+				if dx < 0:
+					vy -= dely
+					vx -= w
+				else:
+					vy += dely
 
-			txt.set_position((v[0, 0], v[0, 1]))
+			txt.set_position((vx, vy))
 
-			#
+
 	def labelAngles(self, labelList):
+		self.labelVertices(labelList, True)
+
+	def labelAnglesOld(self, labelList):
 		# Everything is the counter clockwise, and the first angle/vertex is the first lable, everything else is counter clockwise order
 		# The side that's mentioned first is horizontal
 		self.labels = labelList
