a26cc46dcb020ccbee40229b6ac1b5e5e9f7a1b0
Axel-Jose Persinger <me@ajpersinger.com>
2017-06-26 16:39:01 -0400

    Right angles for polygons

--- shapes/Polygon.py
+++ shapes/Polygon.py
@@ -56,7 +56,7 @@ class Polygon:
 			txt = self.figure.ax.text(0, 0, '$'+label+'$', fontsize=fontsize)
 			txt.set_bbox(dict(facecolor='white', edgecolor='none', pad=0.1))
 
-			v, w, h, wp, hp = self.alignTextAlongVector(txt, v, d, i, debug=True)
+			v, w, h, wp, hp = self.alignTextAlongVector(txt, v, d, i, debug=False)
 
 			txt.set_position((v[0,0], v[0,1]))
 
@@ -70,7 +70,7 @@ class Polygon:
 		bisec = bisec / np.linalg.norm(bisec)
 		return bisec
 
-	def labelVertices(self, labelList, inner=False, fontsize=15, factorPadding=1):
+	def labelVertices(self, labelList, inner=False, fontsize=15, factorPadding=1, arcs=None):
 		# Everything is the counter clockwise, and the first angle/vertex is the first lable, everything else is counter clockwise order
 		# The side that's mentioned first is horizontal
 		if inner:
@@ -94,7 +94,7 @@ class Polygon:
 
 			v = np.copy(self.vertices[i, :])
 
-			v, w, h, wp, hp = self.alignTextAlongVector(txt, v, d, i, debug=True)
+			v, w, h, wp, hp = self.alignTextAlongVector(txt, v, d, i, debug=False)
 
 			# compute necessary padding to clear the polygon
 			if inner:
@@ -107,24 +107,45 @@ class Polygon:
 
 
 			# Adding arcs for angle labels
-			if inner:
-				r = self.figure.UNITS_PER_PT_x * 8
+			if inner and arcs is not None:
+				if isinstance(arcs[i], int):
+					for k in range(arcs[i]):
+						r_x = self.figure.UNITS_PER_PT_x * 8
+						r_y = self.figure.UNITS_PER_PT_y * 8
 
-				# these are just the vectors along the adjacent sides
-				ab = self.vertices[i-1, :] - self.vertices[i, :]
-				ab = ab / np.linalg.norm(ab)
-				ac = self.vertices[(i+1) % self.vertices.shape[0], :] - self.vertices[i, :]
-				ac = ac / np.linalg.norm(ac)
+						# these are just the vectors along the adjacent sides
+						ab = self.vertices[i-1, :] - self.vertices[i, :]
+						ab = ab / np.linalg.norm(ab)
+						ac = self.vertices[(i+1) % self.vertices.shape[0], :] - self.vertices[i, :]
+						ac = ac / np.linalg.norm(ac)
 
 
-				theta1 = np.arctan2(ac[0,1], ac[0, 0]) * 180.0 / np.pi
-				theta2 = np.arctan2(ab[0,1], ab[0, 0]) * 180.0 / np.pi
+						theta1 = np.arctan2(ac[0,1], ac[0, 0]) * 180.0 / np.pi
+						theta2 = np.arctan2(ab[0,1], ab[0, 0]) * 180.0 / np.pi
 
-				self.figure.ax.add_patch(Arc((self.vertices[i,0], self.vertices[i,1]), 2*r, 2*r, theta1=theta1, theta2=theta2, edgecolor='k', linewidth=1))
+						r_x = (2*r_x + k*10*self.figure.UNITS_PER_PIXEL_x)/2
+						r_y = (2*r_y + k*10*self.figure.UNITS_PER_PIXEL_y)/2
 
-				pmin = r + 4*self.figure.UNITS_PER_PT_x
-				if p < pmin:
-					p = pmin
+						#self.figure.ax.add_patch(Arc((self.vertices[i,0] + k*5*self.figure.UNITS_PER_PIXEL_x, self.vertices[i,1] + k*5*self.figure.UNITS_PER_PIXEL_y), 2*r, 2*r, theta1=theta1, theta2=theta2, edgecolor='k', linewidth=1))
+						self.figure.ax.add_patch(Arc((self.vertices[i,0], self.vertices[i,1]), 2*r_x, 2*r_y, theta1=theta1, theta2=theta2, edgecolor='k', linewidth=1))
+
+						pmin = r_x + 4*self.figure.UNITS_PER_PT_x
+						if p < pmin:
+							p = pmin
+				elif arcs[i].upper() == 'SQUARE' or isinstance(arcs[i], str) or arcs[i] < 0:
+					# these are just the vectors along the adjacent sides
+					"""ab = self.vertices[i-1, :] - self.vertices[i, :]
+					ac = self.vertices[(i+1) % self.vertices.shape[0], :] - self.vertices[i, :]"""
+					ab = self.vertices[i-1, :] - self.vertices[i, :]
+					ab = (ab / np.linalg.norm(ab))*(6*self.figure.UNITS_PER_PT_x)
+					ac = self.vertices[(i+1) % self.vertices.shape[0], :] - self.vertices[i, :]
+					ac = (ac / np.linalg.norm(ac))*(6*self.figure.UNITS_PER_PT_x)
+					aa = [2*np.mean([ac, ab], axis=0)[0].tolist()[0], 2*np.mean([ac, ab], axis=0)[0].tolist()[0]]
+
+					verts = [self.vertices[i, :].tolist()[0], ab.tolist()[0], aa, ac.tolist()[0]]
+					cent = np.mean(verts, axis=0)
+					verts.sort(key=lambda p: np.arctan2(p[1]-cent[1],p[0]-cent[0]))
+					self.figure.addPolygon(verts, props={'lw':1})
 
 			if inner:
 				v = v + p*d*factorPadding
