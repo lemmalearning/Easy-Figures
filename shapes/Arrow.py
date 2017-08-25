04d34599782309239738fbdcc76ed8cde01d3c80
AxelPersinger <me@axelp.io>
2017-08-25 15:04:33 -0400

    Zorder for axis works again

--- shapes/Arrow.py
+++ shapes/Arrow.py
@@ -7,7 +7,7 @@ import numpy as np
 
 class Arrow:
 	matplotlib_obj = None
-	def __init__(self, start, end, color, headWidth, headLength, lw, mplprops, clip, figure):
+	def __init__(self, start, end, color, headWidth, headLength, lw, mplprops, clip, add, figure):
 		# x0,y0 start
 		# x1,y1 end
 		# xt,yt headlength point
@@ -36,8 +36,8 @@ class Arrow:
 		l_1 = p_t + np.matrix([-v[0, 1], +v[0, 0]])/v_norm*head_width
 		l_2 = p_t + np.matrix([+v[0, 1], -v[0, 0]])/v_norm*head_width
 		p_t0 = (t-0.01)*start + (1.01-t)*end # One pixel into the triangle to avoid any pixel breaks
-		self.head = self.figure.addPolygon([self.figure.px2unit_c(l_1).tolist()[0], self.figure.px2unit_c(end).tolist()[0], self.figure.px2unit_c(l_2).tolist()[0]], lw=0, mplprops={'color':color}, clip=clip)
-		self.line = self.figure.addLine(self.figure.px2unit_c(start).tolist()[0], self.figure.px2unit_c(p_t0).tolist()[0], lw=lw, color=color, clip=clip)
+		self.head = self.figure.addPolygon([self.figure.px2unit_c(l_1).tolist()[0], self.figure.px2unit_c(end).tolist()[0], self.figure.px2unit_c(l_2).tolist()[0]], lw=0, mplprops={'color':color}, add=add, clip=clip)
+		self.line = self.figure.addLine(self.figure.px2unit_c(start).tolist()[0], self.figure.px2unit_c(p_t0).tolist()[0], lw=lw, color=color, add=add, clip=clip)
 
 	def __draw__(self, zorder=1):
 		self.line.__draw__(zorder=zorder)
